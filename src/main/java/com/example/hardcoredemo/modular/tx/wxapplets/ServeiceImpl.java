f6797eca66bba55f75a26ebb87142bc01c54ae25
zhangshaowei <1123492655@qq.com>
2021-01-05 16:31:11 +0800

    up

--- src/main/java/com/example/hardcoredemo/modular/tx/wxapplets/ServeiceImpl.java
+++ src/main/java/com/example/hardcoredemo/modular/tx/wxapplets/ServeiceImpl.java
@@ -1,14 +1,26 @@
 package com.example.hardcoredemo.modular.tx.wxapplets;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.example.hardcoredemo.common.http.OkHttp3Util;
+import com.example.hardcoredemo.common.http.PostType;
+import org.apache.commons.lang3.StringUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Base64;
 import org.springframework.beans.factory.annotation.Value;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.*;
+import java.security.spec.InvalidParameterSpecException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class ServeiceImpl implements Service{
 
@@ -21,7 +33,7 @@ public class ServeiceImpl implements Service{
     @Override
     public String getAccessToken(){
         String message = OkHttp3Util.get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+appid+"&secret="+secret);
-        return JSONObject.parseObject(message).get("access_token").toString();
+        return JSON.parseObject(message).get("access_token").toString();
     }
 
     @Override
@@ -30,5 +42,56 @@ public class ServeiceImpl implements Service{
         return JSON.parseObject(message, WXUserInfo.class);
     }
 
+    @Override
+    public byte[] getRQCode(String scene){
+        String accessToken = getAccessToken();
+        if(StringUtils.isBlank(accessToken)){return null;}
+        Map<String,Object> params = new HashMap<>();
+        params.put("scene",scene);
+//        params.put("page","pages/index/login");
+//        params.put("width","");
+//        params.put("auto_color","");
+//        params.put("line_color","");
+//        params.put("is_hyaline","");
+        return OkHttp3Util.postStream("https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=" + accessToken,
+                JSON.toJSONString(params), PostType.JSON);
+    }
 
+    @Override
+    public DecryptData decryptData(String encryptedData, String iv, String sessionKey){
+        // 被加密的数据
+        byte[] dataByte = org.bouncycastle.util.encoders.Base64.decode(encryptedData);
+        // 加密秘钥
+        byte[] keyByte = org.bouncycastle.util.encoders.Base64.decode(sessionKey);
+        // 偏移量
+        byte[] ivByte = Base64.decode(iv);
+        try {
+            // 如果密钥不足16位，那么就补足.  这个if 中的内容很重要
+            int base = 16;
+            if (keyByte.length % base != 0) {
+                int groups = keyByte.length / base + (keyByte.length % base != 0 ? 1 : 0);
+                byte[] temp = new byte[groups * base];
+                Arrays.fill(temp, (byte) 0);
+                System.arraycopy(keyByte, 0, temp, 0, keyByte.length);
+                keyByte = temp;
+            }
+            // 初始化
+            Security.addProvider(new BouncyCastleProvider());
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding","BC");
+            SecretKeySpec spec = new SecretKeySpec(keyByte, "AES");
+            AlgorithmParameters parameters = AlgorithmParameters.getInstance("AES");
+            parameters.init(new IvParameterSpec(ivByte));
+            cipher.init(Cipher.DECRYPT_MODE, spec, parameters);// 初始化
+            byte[] resultByte = cipher.doFinal(dataByte);
+            if (null != resultByte && resultByte.length > 0) {
+                String result = new String(resultByte, "UTF-8");
+                return JSON.parseObject(result, DecryptData.class);
+            }
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidParameterSpecException
+                | IllegalBlockSizeException | BadPaddingException | UnsupportedEncodingException
+                | InvalidKeyException | InvalidAlgorithmParameterException | NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- src/main/java/com/example/hardcoredemo/modular/tx/wxapplets/Service.java
+++ src/main/java/com/example/hardcoredemo/modular/tx/wxapplets/Service.java
@@ -1,5 +1,7 @@
 package com.example.hardcoredemo.modular.tx.wxapplets;
 
+import javax.servlet.http.HttpServletResponse;
+
 public interface Service {
 
     /**
@@ -14,4 +16,20 @@ public interface Service {
      * @return WXUserInfo
      */
     WXUserInfo getByCode(String code);
+
+    /**
+     * 动态获取微信小程序码
+     * @param scene 小程序码携带的参数（最大长度32位）
+     * @return 图片流的byte数组
+     */
+    byte[] getRQCode(String scene);
+
+    /**
+     * 解密微信用户信息
+     * @param encryptedData 小程序入参
+     * @param iv 小程序入参
+     * @param sessionKey 必须为微信获取用户信息时的sessionKey，否则解密失败
+     * @return
+     */
+    DecryptData decryptData(String encryptedData, String iv, String sessionKey);
 }
